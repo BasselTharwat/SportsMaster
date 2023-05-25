6d623474e4dfbbf0fc5e0dee98fd404a1425aa73
BasselTharwat <bassel.tharwat01@gmail.com>
2023-05-25 17:29:42 +0300

    push

---
+++ WebApplication/WebApplication/AllRequestsSM.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AllRequestsSM.aspx.cs" Inherits="WebApplication.AllRequestsSM" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form runat="server">
+        <asp:TextBox ID="TextBox1" runat="server" placeholder="Host Club Name"></asp:TextBox>
+        <asp:TextBox ID="TextBox2" runat="server" placeholder="Guest Club Name"></asp:TextBox>
+        <asp:TextBox ID="TextBox3" runat="server" placeholder="Start Time"></asp:TextBox>
+        <asp:Button ID="Button1" runat="server" Text="accept" OnClick="accept_Click" />
+        <asp:Button ID="Button2" runat="server" Text="reject" OnClick="reject_Click" />
+    </form>
+    <asp:Table ID="Table1" runat="server" Width="1311px">
+        <asp:TableRow>
+            <asp:TableCell>Club Representative Name</asp:TableCell>
+            <asp:TableCell>Host Club</asp:TableCell>
+            <asp:TableCell>Guest CLub</asp:TableCell>
+            <asp:TableCell>Start Time</asp:TableCell>
+            <asp:TableCell>End Time</asp:TableCell>
+            <asp:TableCell>Status</asp:TableCell>
+        </asp:TableRow>
+    </asp:Table>
+
+</body>
+</html>
---
+++ WebApplication/WebApplication/AllRequestsSM.aspx.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication
+{
+    public partial class AllRequestsSM : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string connStr = WebConfigurationManager.ConnectionStrings["Sports league"].ToString();
+            //create a new connection
+            SqlConnection conn = new SqlConnection(connStr);
+            string username=Session["user"].ToString();
+            var allReq= new SqlCommand("SELECT * from allRequestsASP WHERE username=@username", conn);
+            allReq.Parameters.AddWithValue("@username", username);
+            conn.Open();
+            SqlDataReader rdr = allReq.ExecuteReader(CommandBehavior.CloseConnection);
+            while(rdr.Read())
+            {
+                string crname= rdr.GetString(rdr.GetOrdinal("CRname"));
+                string hostName = rdr.GetString(rdr.GetOrdinal("hostName"));
+                string guestName = rdr.GetString(rdr.GetOrdinal("guestName"));
+                string startTime = rdr.GetDateTime(rdr.GetOrdinal("startTime"))+"";
+                string endTime = rdr.GetDateTime(rdr.GetOrdinal("endTime")) + "";
+                string status = rdr.GetString(rdr.GetOrdinal("status"));
+                TableRow row = new TableRow();
+                TableCell cell1 = new TableCell();
+                TableCell cell2 = new TableCell();
+                TableCell cell3 = new TableCell();
+                TableCell cell4 = new TableCell();
+                TableCell cell5 = new TableCell();
+                TableCell cell6 = new TableCell();
+                cell1.Text = crname;
+                cell2.Text = hostName;
+                cell3.Text = guestName;
+                cell4.Text = startTime;
+                cell5.Text = endTime;
+                cell6.Text = status;
+                row.Cells.Add(cell1);
+                row.Cells.Add(cell2);
+                row.Cells.Add(cell3);
+                row.Cells.Add(cell4);
+                row.Cells.Add(cell5);
+                row.Cells.Add(cell6);
+                Table1.Rows.Add(row);
+            }
+        }
+
+        protected void accept_Click(object sender, EventArgs e)
+        {
+            string connStr = WebConfigurationManager.ConnectionStrings["Sports league"].ToString();
+            //create a new connection
+            SqlConnection conn = new SqlConnection(connStr);
+            string username = Session["user"].ToString();
+            SqlCommand acceptRequest = new SqlCommand("acceptRequest", conn);
+            acceptRequest.CommandType = CommandType.StoredProcedure;
+            acceptRequest.Parameters.Add(new SqlParameter("@stadium_manager_username", username));
+            acceptRequest.Parameters.Add(new SqlParameter("@hosting_club", TextBox1.Text));
+            acceptRequest.Parameters.Add(new SqlParameter("@competing_club", TextBox2.Text));
+            acceptRequest.Parameters.Add(new SqlParameter("@start", TextBox3.Text));
+            SqlParameter oput = acceptRequest.Parameters.Add("@noExistingReq", SqlDbType.Int);
+            oput.Direction = ParameterDirection.Output;
+            conn.Open();
+            try
+            {
+                acceptRequest.ExecuteNonQuery();
+                conn.Close();
+                if ((int)oput.Value == 1)
+                {
+                    Response.Write("Wrong input please try again later ");
+
+                }
+                else
+                {
+                    Response.Write("done need refresh ;)");
+                }
+            }
+            catch(Exception ex)
+            {
+                Response.Write("Wrong input please try again later ");
+            }
+        }
+
+        protected void reject_Click(object sender, EventArgs e)
+        {
+            string connStr = WebConfigurationManager.ConnectionStrings["Sports league"].ToString();
+            //create a new connection
+            SqlConnection conn = new SqlConnection(connStr);
+            string username = Session["user"].ToString();
+            SqlCommand rejectRequest = new SqlCommand("rejectRequest", conn);
+            rejectRequest.CommandType = CommandType.StoredProcedure;
+            rejectRequest.Parameters.Add(new SqlParameter("@stadium_manager_username", username));
+            rejectRequest.Parameters.Add(new SqlParameter("@hosting_club", TextBox1.Text));
+            rejectRequest.Parameters.Add(new SqlParameter("@competing_club", TextBox2.Text));
+            rejectRequest.Parameters.Add(new SqlParameter("@start", TextBox3.Text));
+            SqlParameter oput = rejectRequest.Parameters.Add("@noExistingReq", SqlDbType.Int);
+            oput.Direction = ParameterDirection.Output;
+
+            conn.Open();
+            try
+            {
+                rejectRequest.ExecuteNonQuery();
+                conn.Close();
+                if ((int)oput.Value == 1)
+                {
+                    Response.Write("Wrong input please try again later ");
+
+                }
+                else
+                {
+                    Response.Write("done need refresh ;)");
+                }
+            }
+            catch (Exception ex)
+            {
+                Response.Write("Wrong input please try again later ");
+            }
+            
+        }
+    }
+}
